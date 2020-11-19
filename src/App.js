2748b6da0c4adfe028238b1fafde9960100a102d
sabbir-anwar <protik26@gmail.com>
2020-11-19 18:35:08 +0600

    movie search input box added && Style

--- src/App.js
+++ src/App.js
@@ -17,10 +17,15 @@ function App() {
   }, []);
 
   return (
-    <div className="movie-container">
-      {movies.length > 0 &&
-        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
-    </div>
+    <>
+      <header>
+        <input className="search" type="search" placeholder="Search..." />
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
